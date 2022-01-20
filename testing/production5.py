c7d7d5a20c8fa1387deceddab46ebdabb0ebba19
jfoder <jaroslaw.foder@gmail.com>
2022-01-20 18:53:30 +0100

    Fix validation

--- testing/production5.py
+++ testing/production5.py
@@ -14,6 +14,22 @@ class MyTestCase(unittest.TestCase):
         G.display()
         self.assertEqual(17, G.get_nodes_number)
 
+    def test_production_valid_graph_with_additional_node(self):
+        G = make_initial_graph()
+        I = Node(label='I', x=1, y=1, level=0)
+        G.add_node(I)
+        G.new_edge(G.get_first_node_with_label("E"), I)
+        G.apply_production(P5)
+        G.display()
+        self.assertEqual(18, G.get_nodes_number)
+
+    def test_production_invalid_graph_without_one_node(self):
+        G = make_initial_graph()
+        G.remove_node(G.get_first_node_with_label('E'))
+        with self.assertRaises(CannotApplyProductionError):
+            G.apply_production(P5)
+            G.display()
+
     def test_production_invalid_graph(self):
         G = Graph()
         i = Node(label='i', x=1, y=1, level=0)
